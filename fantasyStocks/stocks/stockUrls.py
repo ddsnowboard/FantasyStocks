61638aaa6d92c79813d97da535d637515c9ed011
Will Koster <ddsnowboard@gmail.com>
2015-10-03 16:06:59 -0500

    Write instructions, try to change up urlconf.
    The browser wants to send me to index.html, but I need to redirect it to
    index/ instead. I don't seem to be catching the request to just the
    website before the browser catches it. Maybe a real issue was being
    hidden before and my urlconf doesn't actually work like I think it does.

--- fantasyStocks/stocks/stockUrls.py
+++ fantasyStocks/stocks/stockUrls.py
@@ -3,8 +3,10 @@
 from stocks import views
 from django.conf.urls import url
 from django.views.generic.base import RedirectView
+from django.core.urlresolvers import reverse
 
 urlpatterns = [
-        url("index.html", views.index),
-        url("$", RedirectView.as_view(url="index.html", permanent=True)), 
+            url("instructions/", views.instructions, name="instructions"),
+            url("index/", views.index, name="homepage"),
+            url("^/$", RedirectView.as_view(url=reverse("homepage"), permanent=True)), 
         ]
