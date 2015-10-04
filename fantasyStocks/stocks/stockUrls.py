5d1e66a4c3f7b2c32ef7cd56fea543c737e3207a
Will Koster <ddsnowboard@gmail.com>
2015-10-04 18:33:38 -0500

    Fix url issue.
    I'm really stupid. There was a $ where there shouldn't have been one.

--- fantasyStocks/stocks/stockUrls.py
+++ fantasyStocks/stocks/stockUrls.py
@@ -2,10 +2,8 @@
 
 from stocks import views
 from django.conf.urls import url, patterns
-from django.views.generic import RedirectView
-from django.core.urlresolvers import reverse_lazy
 
 urlpatterns = [
-            url("^instructions/$", views.instructions, name="howto"),
+            url("^instructions/$", views.instructions, name="instructions"),
             url("^$", views.index, name="home"), 
         ]
