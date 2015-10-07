88964dc79be1c88f1a197c5a59f342c9b7749ed2
Will Koster <ddsnowboard@gmail.com>
2015-10-06 20:23:40 -0500

    Make some progress on the view.
    I need to make it return a HttpResponse though. oops.

--- fantasyStocks/stocks/views.py
+++ fantasyStocks/stocks/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from stocks import forms
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
-    regForm = forms.RegistrationForm()
-    logForm = forms.LoginForm()
-    return render(request, "index.html", {"loginForm" : logForm,  "registrationForm" : regForm})
+    # If we got here through a submission...
+    if request.method == "POST":
+        if request.POST.get("password1", None):
+            form = forms.RegistrationForm(request.POST)
+            if form.is_valid():
+                user = User.objects.create_user(username=form.cleaned_data["email"], email=form.cleaned_data["email"], password=form.cleaned_data["password"])
+                user.save()
+            else:
+                if form._errors["already_exists"]:
+                    error = form._errors["already_exists"]
+                else:
+                    error = "There was an error with your registration"
+        elif request.POST["password"]:
+            form = forms.LoginForm(request.POST)
+    else:
+        regForm = forms.RegistrationForm()
+        logForm = forms.LoginForm()
+        return render(request, "index.html", {"loginForm" : logForm,  "registrationForm" : regForm})
 def instructions(request):
     return render(request, "instructions.html")
