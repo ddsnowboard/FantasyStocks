1923d7d4e90dd5fb9eff31fc93a1ff0854c49c85
Will Koster <ddsnowboard@gmail.com>
2015-11-16 19:44:46 -0600

    Fix issue with joinFloor
    It was causing an error if you click on the table headers.

--- fantasyStocks/static/joinFloor.js
+++ fantasyStocks/static/joinFloor.js
@@ -1,6 +1,9 @@
 "use strict"
 $(document).ready(function() {
-    $("tr").click(function() {
-        window.location = $(this).find("a").attr("href");
+    $("tr").click(function(event) {
+        if(event.target.nodeName !== "TH")
+        {
+            window.location = $(this).find("a").attr("href");
+        }
     });
 });
