b24aeddde7c7a4cc0ff3208609a1a5a1601853f4
Will Koster <ddsnowboard@gmail.com>
2015-11-24 11:10:21 -0600

    Start writing some design stuff for the trade script

---
+++ fantasyStocks/stocks/templates/trade.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    // I guess here is as good a place as any to do some design. 
+    // So what I'm going to do is first query the DOM for the name of each
+    // StockWidget's value box, which I'll have through the StockWidgets list
+    // that I put in the stockWidget.js file. 
+});
