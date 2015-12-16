95d1de6a4dbe298f09eae82eba49abebba428520
Will Koster <ddsnowboard@gmail.com>
2015-12-16 13:56:01 -0600

    Start writing unit test for scoring
    It's mostly for speed testing, really. Anyway, though, it's probably a
    good idea anyway.
    
    Make more progress on #11.

--- fantasyStocks/stocks/tests.py
+++ fantasyStocks/stocks/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from stocks.models import *
+import json
 
-# Create your tests here.
+class ScoringStressTestCase(TestCase):
+    def setUp(self):
+        with open("./static/stocks.json") as f:
+            available_stocks = [Stock.objects.create(symbol=i["symbol"]) for i in json.load(f)]
+        floor_user = User.objects.create_user("Floor", "floor@floors.net", "flooring")
+        floor_user.save()
+        floor = Floor.objects.create(name="TestingFloor", permissiveness="open")
+        floor_player = Player.objects.create(user=floor_user, points=0, floor=floor)
+        floor.floorPlayer = floor_player
+        floor_player.save()
+        floor.save()
+        for i in range(1000):
+            user = User.objects.create_user("user_{}".format(i), "user_{}@mailmail.mail".format(i), "thePasswordIs{}".format(i))
+            player = Player.objects.create(user=user, floor=floor)
+            player.stocks.add(available_stocks.pop())
+            user.save()
+            player.save()
