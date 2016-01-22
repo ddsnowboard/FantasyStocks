c09469ca66182cfc145c08d5f1e5e09c1a6e0d60
Will Koster <ddsnowboard@gmail.com>
2016-01-21 21:59:17 -0600

    Make ConfirmationBox blur rest of page
    It's not as good as making all the buttons not work, but all the HTML
    buttons don't work, and you can't see anything, so it's not bad. I
    tested it a little, and it seems good.

--- fantasyStocks/static/common.js
+++ fantasyStocks/static/common.js
@@ -45,10 +45,19 @@ function ConfirmationBox(text, buttons)
 {
     // Buttons is an array of objects of the form {text: "button text", func: function() { console.log("Do stuff"); }}
     // Note that the functions are run with the actual ConfirmationBox as the context for using `this`, not the HTML or jQuery button. 
+    this.destroy = function() 
+    {
+        this.$holder.remove();
+        $("body > *:not(.confirmationBox)").css("opacity", 1);
+        $(":submit").prop("disabled", false);
+    };
+
     this.buttons = buttons;
     this.text = text;
     this.$holder = $("<div class=\"confirmationBox\"></div>");
     this.$holder.html(text + "<br />");
+    $("body > *:not(.confirmationBox)").css("opacity", .1);
+    $(":submit").prop("disabled", true);
     for(var i = 0; i < buttons.length; i++)
     {
         var $currButton = $("<div class=\"confirmationButton\"></div>");
@@ -56,8 +65,4 @@ function ConfirmationBox(text, buttons)
         $currButton.click(buttons[i].func.bind(this));
         this.$holder.append($currButton);
     }
-    this.destroy = function() 
-    {
-        this.$holder.remove();
-    };
 }
