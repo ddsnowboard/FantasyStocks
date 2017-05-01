fa09f1ce31c7447af44c47c4621fa8932e128ca7
Will Koster <jameswk2@illinois.edu>
2017-05-01 16:25:23 -0500

    Fix problem in admin
    The pinging admin action was set up wrong.

--- fantasyStocks/stocksApi/admin.py
+++ fantasyStocks/stocksApi/admin.py
@@ -4,12 +4,18 @@ import stocksApi
 def deleteAllSessionIds(admin, request, queryset):
     stocksApi.models.SessionId.objects.all().delete()
 deleteAllSessionIds.short_description = "Delete all session ids"
+
+def pingAndroids(admin, request, queryset):
+    for i in queryset:
+        i.ping("Admin", "This is a test ping")
+pingAndroids.short_description = "Ping"
+
 # Register your models here.
 
 @admin.register(stocksApi.models.AndroidToken)
 class AndroidTokenAdmin(admin.ModelAdmin):
     fields = ("user", "token")
-    actions = [stocksApi.models.AndroidToken.ping]
+    actions = [pingAndroids]
 
 @admin.register(stocksApi.models.SessionId)
 class SessionIdAdmin(admin.ModelAdmin):
